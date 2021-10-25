d24a91ed3a873fdaf6341fbe6f91f939ca2689e7
freeze1 <humera.rk@gmail.com>
2021-10-25 07:17:04 -0700

    Added gitigore file

--- server.py
+++ server.py
@@ -3,11 +3,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     print(url_for('static', filename='bank_icon.ico'))
-#     return render_template('index.html')
-
+@app.route("/")
+def hello_world():
+    return 'Hello World: ECS Standard'
 @app.route("/<username>/<int:post_id>")
 def hello_world(username=None, post_id=None):
     # print(url_for('static', filename='bank_icon.ico'))
